f2bc16ac06f49c1b8bd9252f8224af7d348c255e
SuwillWD <suwilllokhande688@gmail.com>
2024-06-08 16:42:43 +0530

    Fix minor bug in environment setup

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,5 +3,5 @@ const common = require('./webpack.common.js');
 
 module.exports = merge( common, {
     mode: 'development',
-    devtool: 'inline-source-maps',
+    devtool: 'inline-source-map',
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,5 +3,5 @@ const common = require('./webpack.common.js');
 
 module.exports = merge( common, {
     mode: 'production',
-    devtool: 'source-maps',
+    devtool: 'source-map',
 });
